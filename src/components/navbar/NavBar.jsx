527232cc02133f141f7b40117e0afbc45b209331
zlatan-cic <smajicz999@gmail.com>
2023-08-27 18:53:34 +0200

    Navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles import
 import styles from "./NavBar.module.css";
@@ -11,13 +11,42 @@ import {
   AiOutlineClose,
 } from "react-icons/ai";
 
+// import { MagnifyingGlassIcon, UserIcon } from "@heroicons/react/24/outline";
+
 // Images import
-import Logo from '../../images/logo.png'
+import Logo from "../../images/logo.png";
 
 const NavBar = () => {
-  return <header className={styles.navbar}>
-    <img src={Logo} alt="Logo Image" />
-  </header>;
+  const [nav, setNav] = useState(false);
+
+  return (
+    <header className={styles.navbar}>
+      <img src={Logo} alt="Logo Image" />
+      <nav>
+        <ul className={nav ? [styles.menu, styles.active].join(' ') : [styles.menu]}>
+          <li>
+            <a href="#">Learn more</a>
+          </li>
+          <li>
+            <a href="#">Log In</a>
+          </li>
+          <li>
+            <a href="#">Sing Up</a>
+          </li>
+          <li>
+            <AiOutlineSearch size={25} style={{marginTop: "6px"}} />
+          </li>
+          <li>
+            <AiOutlineUser size={25} style={{marginTop: "6px"}} />
+          </li>
+        </ul>
+      </nav>
+      {/* Mobile */}
+      <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
+        {nav ? <AiOutlineClose size={25}/> : <AiOutlineMenu size={25}/>}
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
